0a6bdb80b22b75327881fb2a01c821953eba2abd
Danniel Sousa <danniel.sousa@gmail.com>
2020-06-28 20:33:16 -0300

    Alteraçoes para paginar

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,35 +1,49 @@
+/* eslint-disable no-debugger */
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img
-        v-if="produto.fotos"
-        :src="produto.fotos[0].src"
-        :alt="produto.fotos[0].titulo"
-      />
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p class="descricao">{{ produto.descricao }}</p>
-      <p clas="preco">{{ produto.preco }}</p>
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div class="produto" v-for="(produto, index) in produtos" :key="index">
+        <router-link to="/">
+          <img
+            v-if="produto.fotos"
+            :src="produto.fotos[0].src"
+            :alt="produto.fotos[0].titulo"
+          />
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p class="descricao">{{ produto.descricao }}</p>
+        </router-link>
+      </div>
+       <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina"/>
     </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">Busca sem resultado. Tente buscar outro termo.</p>
+    </div>
+    <div v-else></div>
   </section>
 </template>
 
 <script>
 import { api } from "@/services.js";
-import { serialize } from '@/helpers.js';
+import { serialize } from "@/helpers.js";
+import ProdutosPaginar from '@/components/ProdutosPaginar.vue';
 
 export default {
-  name: "ProdutosListar",
-
+  name: "ProdutosLista",
+  components: {
+    ProdutosPaginar
+  },
   data: () => ({
     produtos: null,
-    produtosPorPagina: 9,
+    produtosPorPagina: 3,
+    produtosTotal: 0
   }),
 
   computed: {
     url() {
       const query = serialize(this.$route.query);
       return `/produto?_limit=${this.produtosPorPagina}${query}`;
-    }
+    },
   },
 
   created() {
@@ -38,18 +52,69 @@ export default {
 
   watch: {
     url() {
-      this.getProdutos()
-    }
+      this.getProdutos();
+    },
   },
 
   methods: {
     getProdutos() {
       api.get(this.url).then((response) => {
+        this.produtosTotal = Number(response.headers['x-total-count']);
         this.produtos = response.data;
+        console.log(this.produtosPorPagina)
+        console.log(this.produtosTotal);
+
       });
     },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  font-weight: bold;
+  color: #e80;
+}
+
+.sem-resultados {
+  text-align: center;
+}
+
+</style>
