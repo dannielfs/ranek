d030ff8462f825f175d03ddd09d50ffa31fc2c54
Danniel Sousa <danniel.sousa@gmail.com>
2020-06-28 13:03:47 -0300

    Serializando a url

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,11 +1,24 @@
 <template>
-  <input type="text">
+  <form action="">
+    <input name="busca" id="busca" v-model="busca" type="text">
+    <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
+    {{busca}}
+  </form>
 </template>
 
 <script>
 
 export default {
-  name: "ProdutosBuscar"
+  name: "ProdutosBuscar",
+  data: () => ({
+    busca: '',
+  }),
+  methods: {
+    buscarProdutos() {
+      this.$router.push({query: { q: this.busca }})
+    }
+  }
+
 }
 
 </script>
--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -15,21 +15,36 @@
 
 <script>
 import { api } from "@/services.js";
+import { serialize } from '@/helpers.js';
 
 export default {
   name: "ProdutosListar",
 
   data: () => ({
     produtos: null,
+    produtosPorPagina: 9,
   }),
+
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    }
+  },
 
   created() {
     this.getProdutos();
   },
 
+  watch: {
+    url() {
+      this.getProdutos()
+    }
+  },
+
   methods: {
     getProdutos() {
-      api.get("/produto").then((response) => {
+      api.get(this.url).then((response) => {
         this.produtos = response.data;
       });
     },
