133591ab506e8c41a1e9e41b86ab56cf9ce0ee5c
Danniel Sousa <danniel.sousa@gmail.com>
2020-06-30 08:54:03 -0300

    Criaçao do formulario de usuario

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -1,15 +1,45 @@
 <template>
-  <p>Login Criar</p>
+  <section>
+    <h2>Crie a sua conta</h2>
+    <transition mode="out-in">
+    <button class="btn criar" v-if="!criar" @click="criar = true">Criar conta</button>
+    <UsuarioForm v-else>
+      <button class="btn btn-form">Criar Usuário</button>
+    </UsuarioForm>
+    </transition>
+  </section>
 </template>
 
 <script>
-
+import UsuarioForm from '@/components/UsuarioForm.vue'
 export default {
-  name: 'LoginCriar'
-}
-
+  name: "LoginCriar",
+  components: {
+    UsuarioForm
+  },
+  data: () => ({
+    criar: false
+  })
+};
 </script>
 
-<style>
+<style scoped>
+h2 {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.btn-form {
+  max-width: 100%;
+}
+
 
 </style>
