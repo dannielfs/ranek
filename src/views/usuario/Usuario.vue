da6160eaed92a240149322fb3903617609d52a76
Danniel Sousa <danniel.sousa@gmail.com>
2020-07-18 10:57:41 -0300

    Criaçao do componente de usuario

--- src/views/usuario/Usuario.vue
+++ src/views/usuario/Usuario.vue
@@ -3,16 +3,18 @@
     <nav class="sidenav">
       <ul>
         <li>
-          <router-link :to="{name: 'usuario'}">Produtos</router-link>
+          <router-link :to="{ name: 'usuario' }">Produtos</router-link>
         </li>
         <li>
-          <router-link :to="{name: 'compras'}">Compras</router-link>
+          <router-link :to="{ name: 'compras' }">Compras</router-link>
         </li>
         <li>
-          <router-link :to="{name: 'vendas'}">Vendas</router-link>
+          <router-link :to="{ name: 'vendas' }">Vendas</router-link>
         </li>
         <li>
-          <router-link :to="{name: 'usuario-editar'}">Editar Usuário</router-link>
+          <router-link :to="{ name: 'usuario-editar' }"
+            >Editar Usuário</router-link
+          >
         </li>
         <li>
           <button @click.prevent="deslogar">Deslogar</button>
@@ -32,10 +34,50 @@ export default {
     deslogar() {
       this.$store.dispatch("deslogarUsuario");
       this.$router.push("/login");
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.usuario {
+  display: grid;
+  grid-template-columns: minmax(140px, 200px) 1fr;
+  max-width: 990px;
+  margin: 40px auto;
+  grid-gap: 30px;
+  padding: 20px;
+}
+
+@media screen and (max-width: 500px) {
+  .usuario {
+    grid-template-columns: 1fr;
+    margin: 0;
+  }
+}
+
+.sidenav a, .sidenav button {
+  padding: 10px;
+  display: block;
+  background: #f4f7fc;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.sidenav a.router-link-exact-active,
+.sidenav a:hover,
+.sidenav button:hover {
+  background: #87f;
+  color: #fff;
+}
+
+.sidenav button {
+  border: none;
+  width: 100%;
+  font-size: 1rem;
+  text-align: left;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  cursor: pointer;
+  color: #345;
+}
 </style>
